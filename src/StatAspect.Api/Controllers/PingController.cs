a0c5c8a5a569751971cac9406e5b5a5b8441a2c8
Evgeniy Datskiy <datskiy-jnk@yandex.ru>
2022-03-01 22:55:10 +0400

    Init.

---
+++ src/StatAspect.Api/Controllers/PingController.cs
@@ -0,0 +1,20 @@
+﻿namespace StatAspect.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public sealed class PingController : ControllerBase
+{
+    public PingController()
+    {
+    }
+
+    /// <summary>
+    /// Checks the API accessibility.
+    /// </summary>
+    /// <returns><see cref="IActionResult"/></returns>
+    [HttpGet]
+    public IActionResult Ping()
+    {
+        return Ok();
+    }
+}
